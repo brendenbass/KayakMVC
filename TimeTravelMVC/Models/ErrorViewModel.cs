f0a6101ad23016893ef574dca4e90fe3f945302d
Brenden Bass <80184025+brendenbass@users.noreply.github.com>
2021-08-02 10:23:29 -0400

    Add project files.

---
+++ TimeTravelMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TimeTravelMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
